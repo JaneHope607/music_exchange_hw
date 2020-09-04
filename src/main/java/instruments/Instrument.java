60cf0ea15487a29a585835528e9832f1c72e5550
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-04 11:17:28 +0100

    IPlay interface set up

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,4 +1,44 @@
 package instruments;
 
 public abstract class Instrument {
+
+    private String type;
+    private String material;
+    private String colour;
+    private int stock;
+    private double buyPrice;
+    private double sellPrice;
+
+    public Instrument(String type, String material, String colour, int stock, double buyPrice, double sellPrice) {
+        this.type = type;
+        this.material = material;
+        this.colour = colour;
+        this.stock = stock;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
 }
--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,7 @@
 package instruments;
 
-public class Piano {
+public class Piano extends Instrument {
+    public Piano(String type, String material, String colour, int stock, double buyPrice, double sellPrice) {
+        super(type, material, colour, stock, buyPrice, sellPrice);
+    }
 }
