1f236dd382b315f05207294deb03bc783325bef9
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-04 11:24:24 +0100

    ISell interface completed

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -5,15 +5,13 @@ public abstract class Instrument {
     private String type;
     private String material;
     private String colour;
-    private int stock;
     private double buyPrice;
     private double sellPrice;
 
-    public Instrument(String type, String material, String colour, int stock, double buyPrice, double sellPrice) {
+    public Instrument(String type, String material, String colour, double buyPrice, double sellPrice) {
         this.type = type;
         this.material = material;
         this.colour = colour;
-        this.stock = stock;
         this.buyPrice = buyPrice;
         this.sellPrice = sellPrice;
     }
@@ -28,10 +26,6 @@ public abstract class Instrument {
 
     public String getColour() {
         return colour;
-    }
-
-    public int getStock() {
-        return stock;
     }
 
     public double getBuyPrice() {
--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,7 +1,8 @@
 package instruments;
 
 public class Piano extends Instrument {
-    public Piano(String type, String material, String colour, int stock, double buyPrice, double sellPrice) {
-        super(type, material, colour, stock, buyPrice, sellPrice);
+
+    public Piano(String type, String material, String colour, double buyPrice, double sellPrice) {
+        super(type, material, colour, buyPrice, sellPrice);
     }
 }
